ddbd5dcd7ded9233a7d7a56873eed2987a811dfd
Cali <agustincalisaya01@gmail.com>
2026-08-16 03:27:56 -0300

    Estructura de Directorio

--- src/components/inventario/(borrar).tsx
+++ src/components/inventario/(borrar).tsx
@@ -1,3 +1,5 @@
-export default function LoginPage() {
-  return <div>Login</div>;
+// BORRAR ANTES DE PROGRAMAR 
+
+export default function PlaceholderLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
 }
